ba58a6cbbd9cd15549032edf0f767306f31059c5
Cristian Santos <csantosr@unal.edu.co>
2023-06-18 21:28:34 -0500

    Adding endpoints to get user and find by email

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +9,15 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDTO) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findUser(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
