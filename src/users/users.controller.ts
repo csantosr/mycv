f74d4560bc7ab5446831713f26e525802cfa5ed2
Cristian Santos <csantosr@unal.edu.co>
2023-06-19 09:44:11 -0500

    Good serializer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,13 +8,14 @@ import {
   Delete,
   Patch,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dtos/update-user.dto';
+import { UserDTO } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
+@Serialize(UserDTO)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -24,7 +25,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findUser(parseInt(id));
