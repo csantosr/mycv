320de434104bc4703cfd3d9d5ab9ba19eea83110
Cristian Santos <csantosr@unal.edu.co>
2023-06-19 21:47:08 -0500

    Test signin updates session object and returns user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -33,7 +33,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -72,5 +74,16 @@ describe('UsersController', () => {
     fakeUsersService.findUser = () => Promise.resolve(null);
 
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -1 };
+    const user = await controller.signin(
+      { email: 'asd@sadf.com', password: 'asdf' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
