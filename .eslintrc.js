7380a599f06ecd9b10bff764c3181d4146a13779
Cristian Santos <csantosr@unal.edu.co>
2023-06-21 17:53:04 -0500

    Creating middlewares

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     "no-unused-vars": "off",
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn", // or "error"
       { 
