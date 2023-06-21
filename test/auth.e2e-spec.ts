a5362db7e012f55acdb57948164e78af2b8e05fb
Cristian Santos <csantosr@unal.edu.co>
2023-06-20 19:38:29 -0500

    signup as a new user then get the currently logged in user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,21 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = `${Math.round(Math.random() * 1000)}@unique.com`;
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'unique' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
