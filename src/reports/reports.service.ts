88d890333de5cb75abdfa59838dd709552fb1fb4
Cristian Santos <csantosr@unal.edu.co>
2023-06-20 20:33:06 -0500

    Create report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDTO } from './dtos/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDTO: CreateReportDTO) {
+    const report = this.repo.create(reportDTO);
+
+    return report;
+  }
+}
