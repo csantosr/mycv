0052921eb6406141a65bc6f0c1bba6465fe8247a
Cristian Santos <csantosr@unal.edu.co>
2023-06-20 21:05:23 -0500

    Save user on report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,13 +3,15 @@ import { CreateReportDTO } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDTO: CreateReportDTO) {
+  create(reportDTO: CreateReportDTO, user: User) {
     const report = this.repo.create(reportDTO);
+    report.user = user;
 
     return report;
   }
